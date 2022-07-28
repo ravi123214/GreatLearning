aaf66aadf91299299ef3c3521d85ce5bd0375210
Ravi <ravivns2000@gmail.com>
2022-07-28 13:23:51 +0530

    initial commit

---
+++ src/main/java/com/greatlearning/DataBase1Application.java
@@ -0,0 +1,13 @@
+package com.greatlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBase1Application.class, args);
+	}
+
+}
